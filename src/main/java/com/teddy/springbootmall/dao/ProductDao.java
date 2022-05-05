96f768058240df22f088cd683c4825edd1276e74
TeddyWalk <teddyw0118@gmail.com>
2022-05-05 11:41:49 +0800

    商品功能 — 實作查詢商品

---
+++ src/main/java/com/teddy/springbootmall/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.teddy.springbootmall.dao;
+
+import com.teddy.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/teddy/springbootmall/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.teddy.springbootmall.service;
+
+import com.teddy.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer productId);
+}
