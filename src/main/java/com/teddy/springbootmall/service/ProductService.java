1e398a386d70acc8e6db512d47c68dd297611423
TeddyWalk <teddyw0118@gmail.com>
2022-05-05 14:08:57 +0800

    商品功能 — 實作新增商品

--- src/main/java/com/teddy/springbootmall/service/ProductService.java
+++ src/main/java/com/teddy/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.teddy.springbootmall.service;
 
+import com.teddy.springbootmall.dto.ProductRequest;
 import com.teddy.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
