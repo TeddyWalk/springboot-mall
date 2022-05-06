f25e04bb8c0a48a06539152f4fb9c8d2b88792f3
TeddyWalk <teddyw0118@gmail.com>
2022-05-06 11:29:32 +0800

    商品功能 — 實作查詢商品列表(排序)

--- src/main/java/com/teddy/springbootmall/controller/ProductController.java
+++ src/main/java/com/teddy/springbootmall/controller/ProductController.java
@@ -21,12 +21,19 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue ="created_date") String orderBy,  // 根據什麼來排序
+            @RequestParam(defaultValue ="DESC") String sort      // 用升序還降序
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
